babd11912ab76dbe24a35fc736b09009499551fc
Adrian Ściepura <adriansciepura@outlook.com>
2023-12-31 02:30:25 +0100

    Updated Dialog System (#3)
    Display message by letter

--- Assets/Scripts/Managers/UI/DialogUIController.cs
+++ Assets/Scripts/Managers/UI/DialogUIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -17,10 +18,14 @@ public class DialogUIController : MonoBehaviour
     [SerializeField] 
     private Image _speakerImage;
 
+    private DialogueInteractionItem _currentDialogue;
+    private Coroutine _currentCoroutine;
+
 
     private void Start()
     {
         _canvas.SetActive(false);
+        _currentCoroutine = null;
         GameDataManager.input.Overlay.ContinueDialogue.performed += CloseDialogue;
         EventManager.Instance.Subscribe<OnInteractionItemStartEvent<DialogueInteractionItem>>(DialogueEvent);
     }
@@ -40,10 +45,16 @@ public class DialogUIController : MonoBehaviour
 
         GameDataManager.input.Player.Disable();
         GameDataManager.input.Overlay.Enable();
+
+        _currentDialogue = dialogueInteraction.data;
+
         _canvas.SetActive(true);
         _senderNameText.text = GameDataManager.entityRegistry[sender.EntityType].displayName;
-        _messageText.text = dialogueInteraction.data.message;
         _speakerImage.sprite = sender.GetComponent<SpriteRenderer>().sprite;
+        _messageText.text = string.Empty;
+
+        if(_currentCoroutine == null)
+            _currentCoroutine = StartCoroutine(WriteText());
     }
 
     private void CloseDialogue(InputAction.CallbackContext context)
@@ -51,9 +62,31 @@ public class DialogUIController : MonoBehaviour
         if (!_canvas.activeSelf)
             return;
 
+        if (_currentCoroutine != null)
+        {
+            StopCoroutine(_currentCoroutine);
+            _messageText.text = _currentDialogue.message;
+            _currentCoroutine = null;
+            return;
+        }
+        
         _canvas.SetActive(false);
+
         GameDataManager.input.Overlay.Disable();
         GameDataManager.input.Player.Enable();
         EventManager.Instance.Publish(new OnInteractionItemFinishEvent<DialogueInteractionItem>());
+    }
+
+    private IEnumerator WriteText()
+    {
+        int index = 0;
+        while(index < _currentDialogue.message.Length) 
+        {
+            _messageText.text += _currentDialogue.message[index];
+            index++;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _currentCoroutine = null;
     }
 }
