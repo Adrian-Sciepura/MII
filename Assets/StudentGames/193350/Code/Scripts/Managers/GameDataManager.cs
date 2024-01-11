22f6b406dd263173295f3b2ff69aeed615c5af7c
Adrian Ściepura <adriansciepura@outlook.com>
2024-01-11 01:16:46 +0100

    File cleanup (#5)

---
+++ Assets/StudentGames/193350/Code/Scripts/Managers/GameDataManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameDataManager
+{
+    private static bool setup = false;
+    public static readonly PlayerInputController input = new PlayerInputController();
+    public static readonly Dictionary<ItemType, ItemPrefabSO> itemRegistry = new Dictionary<ItemType, ItemPrefabSO>();
+    public static readonly Dictionary<(string resourceName, Type resourceType), object> resourcesRegistry = new Dictionary<(string, Type), object>();
+
+    public static void InitGameData()
+    {
+        if (setup)
+            return;
+
+        ItemPrefabSO[] itemPrefabs = Resources.LoadAll<ItemPrefabSO>("193350/Item");
+
+        foreach (var itemPrefab in itemPrefabs)
+        {
+            if (itemPrefab.Validate())
+            {
+                ItemType itemType;
+                if (Enum.TryParse(itemPrefab.itemName, out itemType) &&
+                    !itemRegistry.ContainsKey(itemType))
+                {
+                    itemRegistry.Add(itemType, itemPrefab);
+                }
+            }
+        }
+
+        GameObject[] prefabs = Resources.LoadAll<GameObject>("193350/Prefabs");
+
+        foreach (var prefab in prefabs)
+            resourcesRegistry.Add((prefab.name, typeof(GameObject)), prefab);
+
+        Material[] materials = Resources.LoadAll<Material>("193350/Materials");
+
+        foreach (var material in materials)
+            resourcesRegistry.Add((material.name, typeof(Material)), material);
+
+        setup = true;
+    }
+}
